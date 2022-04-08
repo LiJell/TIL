5a674930689a06acdc530071001975a5a4b6f566
LiJell <hanjukim0922@gmail.com>
2022-04-08 17:49:58 +0900

    React_practice

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from "react";
-
+import "./App.css";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import About from 
+// react component life cycle 찾아보기
 class App extends Component {
-  state = {
-    count: 0,
-  };
-  // 불변성 유지를 위해 값을 변경해줘야함
-  plusOne = () => {
-    this.setState((current) => ({
-      count: current.count + 1,
-    }));
-  };
-  minusOne = () => {
-    this.setState((current) => ({
-      count: current.count - 1,
-    }));
-  };
 
   render() {
     return (
       <div>
-        <div>현재값은 : {this.state.count}</div>;
-        <button onClick={this.plusOne}>더하기</button>
-        <button onClick={this.minusOne}>빼기</button>
+        <div>현재값은</div>
+        
       </div>
     );
   }
