248f601e09878faa5c6f49076af8aed335c63a0d
LiJell <hanjukim0922@gmail.com>
2022-04-08 14:47:47 +0900

    props_state

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,60 +1,30 @@
 import React, { Component } from "react";
 
-function Food({ id, name, image, rating }) {
-  return (
-    <div>
-      <div> I like {name}</div>
-      <img src= {image} />
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    count: 0,
+  };
+  // 불변성 유지를 위해 값을 변경해줘야함
+  plusOne = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  minusOne = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 5,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 5,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} image={dish.image} />
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <div>현재값은 : {this.state.count}</div>;
+        <button onClick={this.plusOne}>더하기</button>
+        <button onClick={this.minusOne}>빼기</button>
+      </div>
+    );
+  }
 }
 
 export default App;
