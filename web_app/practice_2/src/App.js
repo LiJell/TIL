5687a21d3c1cfa8dc943c2e2e55781e830358025
LiJell <hanjukim0922@gmail.com>
2022-04-15 10:52:31 +0900

    react_practice

---
+++ web_app/practice_2/src/App.js
@@ -0,0 +1,38 @@
+// import React, { Component, Fragment } from "react";
+
+// function App() {
+//   return (
+//     <Fragment>
+//       <div>asdsad</div>
+//       <div>asdsad</div>
+//     </Fragment>
+//   );
+// }
+
+// or
+
+import React, { Component } from "react";
+
+// function App() {
+//   return (
+//     <div>
+//       <div>asdsad</div>
+//       <div>asdsad</div>
+//     </div>
+//   );
+// }
+
+// or
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>asdsad</div>
+        <div>asdsad</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ web_app/template/src/Number.js
@@ -0,0 +1,7 @@
+import React, {Component} from "react";
+
+class Number extends Component {
+    return (
+        
+    )
+}
